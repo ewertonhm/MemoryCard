a81c57e242532cbb475e348060299c32528e8984
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-10-22 12:33:16 -0300

    initial commit

---
+++ Seleciona Cartas/Assets/Scripts/Counter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] int playTime;
+    [SerializeField] string nextView;
+    [SerializeField] float timeLeft;
+
+    [SerializeField] GameObject counterDisplayText;
+
+    private void timeOver()
+    {
+        SceneManager.LoadScene(nextView);
+    }
+
+    public string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
+        return string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliseconds);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = (float)playTime;
+        counterDisplayText.GetComponent<TextMeshProUGUI>().text = playTime.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        counterDisplayText.GetComponent<TextMeshProUGUI>().text = FormatTime(timeLeft);
+        if (timeLeft <= 0)
+        {
+            timeOver();
+        }
+    }
+}
---
+++ Seleciona Cartas/Assets/Scripts/Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Vamos ver do que você é capaz");
+        Debug.Log("Escolha o Tempo");
+        Debug.Log("Escolha o Número de cartas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
