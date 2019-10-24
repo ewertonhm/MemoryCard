d94e51c2d10ec448f028990aacc75b95bfd71518
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-10-24 16:31:30 -0300

    card select

--- Seleciona Cartas/Assets/Scripts/CardSelect.cs
+++ Seleciona Cartas/Assets/Scripts/CardSelect.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CardSelect : MonoBehaviour
 {
     [SerializeField] GameObject[] cartas;
+    private int numberOfCards;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +13,11 @@ public class CardSelect : MonoBehaviour
         {
             carta.SetActive(false);
         }
-        
+        this.numberOfCards = GameConfigs.instance.getNumberOfCards();
+        for(int c = 0 ; c<numberOfCards ; c++)
+        {
+            cartas[c].SetActive(true);
+        }
     }
 
     // Update is called once per frame
