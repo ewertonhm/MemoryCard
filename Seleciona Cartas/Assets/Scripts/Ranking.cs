8d02b6c0511be367a4f3e7744d7403c5712b0d6e
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2019-11-21 16:54:59 -0300

    playerprefs

--- Seleciona Cartas/Assets/Scripts/Ranking.cs
+++ Seleciona Cartas/Assets/Scripts/Ranking.cs
@@ -9,6 +9,7 @@ public class Ranking : MonoBehaviour
     [SerializeField] List<int> acertos = new List<int>();
     [SerializeField] List<int> numeroDeCartas = new List<int>();
     [SerializeField] List<int> tempo = new List<int>();
+    [SerializeField] List<int> pontos = new List<int>();
 
 
     public string getNome(int posicao)
@@ -68,12 +69,63 @@ public class Ranking : MonoBehaviour
         return this.tempo;
     }
 
-
+    public void lerPlayerPrefsNome()
+    {
+        int c = 0;
+        while(PlayerPrefs.GetString("nome"+c.ToString(), "fim") != "fim")
+        {
+            setNome(PlayerPrefs.GetString("nome" + c.ToString()));
+            c++;
+        }
+    }
+    public void guardarPlayerPrefsNome()
+    {
+        for(int i = 0; i<this.nome.Count; i++)
+        {
+            PlayerPrefs.SetString("nome" + i, this.nome[i]);
+        }
+    }
+    public void lerPlayerPrefsAcertos()
+    {
+        int c = 0;
+        while (PlayerPrefs.GetString("nome" + c.ToString(), "fim") != "fim")
+        {
+            setNome(PlayerPrefs.GetString("nome" + c.ToString()));
+            c++;
+        }
+    }
+    public void lerPlayerPrefsNumeroDeCartas()
+    {
+        int c = 0;
+        while (PlayerPrefs.GetString("nome" + c.ToString(), "fim") != "fim")
+        {
+            setNome(PlayerPrefs.GetString("nome" + c.ToString()));
+            c++;
+        }
+    }
+    public void lerPlayerPrefsTempo()
+    {
+        int c = 0;
+        while (PlayerPrefs.GetString("nome" + c.ToString(), "fim") != "fim")
+        {
+            setNome(PlayerPrefs.GetString("nome" + c.ToString()));
+            c++;
+        }
+    }
+    public void lerPlayerPrefsPontos()
+    {
+        int c = 0;
+        while (PlayerPrefs.GetString("nome" + c.ToString(), "fim") != "fim")
+        {
+            setNome(PlayerPrefs.GetString("nome" + c.ToString()));
+            c++;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.lerPlayerPrefsNome();
     }
 
     // Update is called once per frame
